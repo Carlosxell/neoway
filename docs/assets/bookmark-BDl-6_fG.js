cecc089ea25eccbc1082e8758ae681b3acb2ebe9
carlos augusto <carlos.xell.3@gmail.com>
2024-08-01 19:44:58 -0300

    FEATURE: removed docs directory

--- docs/assets/bookmark-BDl-6_fG.js
+++
@@ -1,1 +0,0 @@
-import{o as e,a as o,e as t}from"./index-Yu0TliJx.js";const s={xmlns:"http://www.w3.org/2000/svg",viewBox:"0 -960 960 960"},n=t("path",{d:"M200-120v-640q0-33 23.5-56.5T280-840h400q33 0 56.5 23.5T760-760v640L480-240zm80-122 200-86 200 86v-518H280zm0-518h400z"},null,-1),r=[n];function a(c,d){return e(),o("svg",s,[...r])}const _={render:a};export{_ as default,a as render};
